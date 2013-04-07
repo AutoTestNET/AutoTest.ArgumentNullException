93704ce814fc5788345f143fb2f438869c4d4e65
James Skimming <gitcommits@skimming.net>
2013-04-07 16:30:53 +0200

    Applied StyleCop recommendations

--- src/AutoTest.ArgumentNullException/RequiresArgumentNullExceptionAttribute.cs
+++ src/AutoTest.ArgumentNullException/RequiresArgumentNullExceptionAttribute.cs
@@ -25,7 +25,7 @@
         /// Initializes a new instance of the <see cref="RequiresArgumentNullExceptionAttribute"/> class.
         /// </summary>
         /// <param name="typeInAssembly">A type in the assembly under test.</param>
-        protected RequiresArgumentNullExceptionAttribute(
+        public RequiresArgumentNullExceptionAttribute(
             Type typeInAssembly)
             : this(new AutoDataAttribute(), typeInAssembly.Assembly)
         {
@@ -47,79 +47,6 @@
         }
 
         /// <summary>
-        /// Determines if a <paramref name="type"/> is compiler generated.
-        /// </summary>
-        /// <param name="type">The type.</param>
-        /// <returns><c>true</c> if the type was compiler generated; otherwise <c>false</c>.</returns>
-        static bool IsCompilerGenerated(MemberInfo type)
-        {
-            if (type == null) throw new ArgumentNullException("type");
-
-            if (type.GetCustomAttribute<CompilerGeneratedAttribute>() != null)
-                return true;
-
-            return type.DeclaringType != null && IsCompilerGenerated(type.DeclaringType);
-        }
-
-        /// <summary>
-        /// Executes the <paramref name="methodUnderTest"/> synchronously.
-        /// </summary>
-        /// <param name="methodUnderTest">The method information.</param>
-        /// <param name="sut">The system under tests, can be <c>null</c> if the <paramref name="methodUnderTest"/> is static.</param>
-        /// <param name="parameters">The parameters to the <paramref name="methodUnderTest"/>.</param>
-        private static async Task ExecuteAsynchronously(MethodBase methodUnderTest, object sut, object[] parameters)
-        {
-            try
-            {
-                var result = (Task)methodUnderTest.Invoke(sut, parameters);
-                await result;
-            }
-            catch (TargetInvocationException targetInvocationException)
-            {
-                throw targetInvocationException.InnerException;
-            }
-        }
-
-        /// <summary>
-        /// Executes the <paramref name="methodUnderTest"/> synchronously.
-        /// </summary>
-        /// <param name="methodUnderTest">The method information.</param>
-        /// <param name="sut">The system under tests, can be <c>null</c> if the <paramref name="methodUnderTest"/> is static.</param>
-        /// <param name="parameters">The parameters to the <paramref name="methodUnderTest"/>.</param>
-        private static void ExecuteSynchronously(MethodBase methodUnderTest, object sut, object[] parameters)
-        {
-            try
-            {
-                methodUnderTest.Invoke(sut, parameters);
-            }
-            catch (TargetInvocationException targetInvocationException)
-            {
-                throw targetInvocationException.InnerException;
-            }
-        }
-
-        private void SetupExecutingAction(MethodData methodData)
-        {
-            if (methodData == null) throw new ArgumentNullException("methodData");
-
-            object sut = null;
-            if (!methodData.MethodUnderTest.IsStatic)
-            {
-                var context = new SpecimenContext(AutoDataAttribute.Fixture);
-                sut = context.Resolve(new SeededRequest(methodData.ClassUnderTest, null));
-            }
-
-            if (methodData.MethodUnderTest.ReturnType.IsSubclassOf(typeof(Task)))
-            {
-                methodData.ExecutingActionAsync = () => ExecuteAsynchronously(methodData.MethodUnderTest, sut, methodData.Arguments);
-            }
-            else
-            {
-                methodData.ExecutingActionSync = () => ExecuteSynchronously(methodData.MethodUnderTest, sut, methodData.Arguments);
-            }
-        }
-
-        /// <summary>
         /// Returns the data for the test <see cref="TheoryAttribute"/>.
         /// </summary>
         /// <param name="methodUnderTest">The test method under test.</param>
@@ -138,6 +65,7 @@
                     if (method.Name == "Equals") continue;
 
                     ParameterInfo[] parameterInfos = method.GetParameters();
+
                     // If there are no parameters, or all the parameters are defaulted to null.
                     if (parameterInfos.Length == 0 || parameterInfos.All(pi => pi.HasDefaultValue && pi.DefaultValue == null)) continue;
 
@@ -176,6 +104,84 @@
             }
 
             return data.Select(d => new object[] { d });
+        }
+
+        /// <summary>
+        /// Determines if a <paramref name="type"/> is compiler generated.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns><c>true</c> if the type was compiler generated; otherwise <c>false</c>.</returns>
+        private static bool IsCompilerGenerated(MemberInfo type)
+        {
+            if (type == null) throw new ArgumentNullException("type");
+
+            if (type.GetCustomAttribute<CompilerGeneratedAttribute>() != null)
+                return true;
+
+            return type.DeclaringType != null && IsCompilerGenerated(type.DeclaringType);
+        }
+
+        /// <summary>
+        /// Executes the <paramref name="methodUnderTest"/> synchronously.
+        /// </summary>
+        /// <param name="methodUnderTest">The method information.</param>
+        /// <param name="sut">The system under tests, can be <c>null</c> if the <paramref name="methodUnderTest"/> is static.</param>
+        /// <param name="parameters">The parameters to the <paramref name="methodUnderTest"/>.</param>
+        /// <returns>The <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+        private static async Task ExecuteAsynchronously(MethodBase methodUnderTest, object sut, object[] parameters)
+        {
+            try
+            {
+                var result = (Task)methodUnderTest.Invoke(sut, parameters);
+                await result;
+            }
+            catch (TargetInvocationException targetInvocationException)
+            {
+                throw targetInvocationException.InnerException;
+            }
+        }
+
+        /// <summary>
+        /// Executes the <paramref name="methodUnderTest"/> synchronously.
+        /// </summary>
+        /// <param name="methodUnderTest">The method information.</param>
+        /// <param name="sut">The system under tests, can be <c>null</c> if the <paramref name="methodUnderTest"/> is static.</param>
+        /// <param name="parameters">The parameters to the <paramref name="methodUnderTest"/>.</param>
+        private static void ExecuteSynchronously(MethodBase methodUnderTest, object sut, object[] parameters)
+        {
+            try
+            {
+                methodUnderTest.Invoke(sut, parameters);
+            }
+            catch (TargetInvocationException targetInvocationException)
+            {
+                throw targetInvocationException.InnerException;
+            }
+        }
+
+        /// <summary>
+        /// Sets up either the <see cref="MethodData.ExecutingActionSync"/> or the <see cref="MethodData.ExecutingActionAsync"/> for the method invocation.
+        /// </summary>
+        /// <param name="methodData">The method data.</param>
+        private void SetupExecutingAction(MethodData methodData)
+        {
+            if (methodData == null) throw new ArgumentNullException("methodData");
+
+            object sut = null;
+            if (!methodData.MethodUnderTest.IsStatic)
+            {
+                var context = new SpecimenContext(AutoDataAttribute.Fixture);
+                sut = context.Resolve(new SeededRequest(methodData.ClassUnderTest, null));
+            }
+
+            if (methodData.MethodUnderTest.ReturnType.IsSubclassOf(typeof(Task)))
+            {
+                methodData.ExecutingActionAsync = () => ExecuteAsynchronously(methodData.MethodUnderTest, sut, methodData.Arguments);
+            }
+            else
+            {
+                methodData.ExecutingActionSync = () => ExecuteSynchronously(methodData.MethodUnderTest, sut, methodData.Arguments);
+            }
         }
     }
 }
