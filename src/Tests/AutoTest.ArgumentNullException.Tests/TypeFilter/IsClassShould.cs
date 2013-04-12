7a8d99eaa4a3f776d34357e610fac0ebcf7633d5
James Skimming <gitcommits@skimming.net>
2013-04-12 17:12:16 +0100

    Added more unit tests

---
+++ src/Tests/AutoTest.ArgumentNullException.Tests/TypeFilter/IsClassShould.cs
@@ -0,0 +1,14 @@
+﻿namespace AutoTest.ArgNullEx.TypeFilter
+{
+    using Xunit;
+    using Xunit.Extensions;
+
+    public class IsClassShould
+    {
+        [Theory, AutoMock]
+        public void ReturnName(IsClass sut)
+        {
+            Assert.Equal("IsClass", sut.Name);
+        }
+    }
+}
---
+++ src/Tests/AutoTest.ArgumentNullException.Tests/TypeFilter/NotCompilerGeneratedShould.cs
@@ -0,0 +1,33 @@
+﻿namespace AutoTest.ArgNullEx.TypeFilter
+{
+    using System;
+    using Xunit;
+    using Xunit.Extensions;
+
+    public class NotCompilerGeneratedShould
+    {
+        [Theory, AutoMock]
+        public void ReturnName(NotCompilerGenerated sut)
+        {
+            Assert.Equal("NotCompilerGenerated", sut.Name);
+        }
+
+        [Theory]
+        [InlineData(typeof(NullExtensionsShould.OuterCg), false)]
+        [InlineData(typeof(NullExtensionsShould.OuterCg.InnerCgOuterCg), false)]
+        [InlineData(typeof(NullExtensionsShould.OuterCg.InnerNoCgOuterCg), false)]
+        [InlineData(typeof(NullExtensionsShould.OuterNoCg), true)]
+        [InlineData(typeof(NullExtensionsShould.OuterNoCg.InnerCgOuterNoCg), false)]
+        [InlineData(typeof(NullExtensionsShould.OuterNoCg.InnerNoCgOuterNoCg), true)]
+        public void ExcludeCompilerGeneratedTypes(Type type, bool expected)
+        {
+            ITypeFilter sut = new NotCompilerGenerated();
+
+            // Act
+            bool actual = sut.IncludeType(type);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
