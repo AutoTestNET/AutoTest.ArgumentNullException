4e6c292491a341240e30153fbfaebddcfa6ef08d
James Skimming <gitcommits@skimming.net>
2013-04-13 20:56:16 +0100

    Added execution components

---
+++ src/AutoTest.ArgumentNullException/Execution/DefaultExecutionSetup.cs
@@ -0,0 +1,23 @@
+﻿namespace AutoTest.ArgNullEx.Execution
+{
+    using System;
+    using System.Reflection;
+
+    /// <summary>
+    /// The default <see cref="IExecutionSetup"/>.
+    /// </summary>
+    public class DefaultExecutionSetup : IExecutionSetup
+    {
+        /// <summary>
+        /// Sets up a reflected asynchronous <see cref="MethodInfo"/> execution.
+        /// </summary>
+        /// <param name="methodData">The method data.</param>
+        /// <returns>A reflected asynchronous <see cref="MethodInfo"/> execution.</returns>
+        IExecution IExecutionSetup.Setup(MethodData methodData)
+        {
+            if (methodData == null) throw new ArgumentNullException("methodData");
+
+            return new DefaultExecution(methodData.MethodUnderTest, methodData.Arguments, methodData.InstanceUnderTest);
+        }
+    }
+}
---
+++ src/AutoTest.ArgumentNullException/IExecutionSetup.cs
@@ -0,0 +1,17 @@
+﻿namespace AutoTest.ArgNullEx
+{
+    using System.Reflection;
+
+    /// <summary>
+    /// Defined a setup for a reflected asynchronous <see cref="MethodInfo"/> execution.
+    /// </summary>
+    public interface IExecutionSetup
+    {
+        /// <summary>
+        /// Sets up a reflected asynchronous <see cref="MethodInfo"/> execution.
+        /// </summary>
+        /// <param name="methodData">The method data.</param>
+        /// <returns>A reflected asynchronous <see cref="MethodInfo"/> execution.</returns>
+        IExecution Setup(MethodData methodData);
+    }
+}
