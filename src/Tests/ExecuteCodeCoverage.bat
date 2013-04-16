69101a57500b3e3587b70d185570eba07cc2655f
James Skimming <gitcommits@skimming.net>
2013-04-16 23:24:18 +0100

    Added code coverage

---
+++ src/Tests/ExecuteCodeCoverage.bat
@@ -0,0 +1,15 @@
+﻿::C:\git\AutoTest.ArgumentNullException\src\packages\OpenCover.4.5.1403\OpenCover.Console.exe
+::-register:user
+::"-target:C:\git\AutoTest.ArgumentNullException\src\packages\xunit.runners.1.9.1\tools\xunit.console.clr4.exe"
+::"-targetargs:C:\git\AutoTest.ArgumentNullException\src\Tests\Tests.xunit"
+::"-filter:+[AutoTest.ArgumentNullException]* +[AutoTest.ExampleLibrary]*"
+::"-output:C:\git\AutoTest.ArgumentNullException\src\Tests\CoverageResults.xml"
+
+C:\git\AutoTest.ArgumentNullException\src\packages\OpenCover.4.5.1403\OpenCover.Console.exe -register:user "-target:C:\git\AutoTest.ArgumentNullException\src\packages\xunit.runners.1.9.1\tools\xunit.console.clr4.exe" "-targetargs:C:\git\AutoTest.ArgumentNullException\src\Tests\Tests.xunit" "-filter:+[AutoTest.ArgumentNullException]* +[AutoTest.ExampleLibrary]*" "-output:C:\git\AutoTest.ArgumentNullException\src\Tests\CoverageResults.xml"
+
+::C:\git\AutoTest.ArgumentNullException\src\packages\ReportGenerator.1.8.1.0\ReportGenerator.exe
+::"C:\git\AutoTest.ArgumentNullException\src\Tests\CoverageResults.xml"
+::"C:\git\AutoTest.ArgumentNullException\src\Tests\Report"
+::Html
+
+C:\git\AutoTest.ArgumentNullException\src\packages\ReportGenerator.1.8.1.0\ReportGenerator.exe "C:\git\AutoTest.ArgumentNullException\src\Tests\CoverageResults.xml" "C:\git\AutoTest.ArgumentNullException\src\Tests\Report" Html
