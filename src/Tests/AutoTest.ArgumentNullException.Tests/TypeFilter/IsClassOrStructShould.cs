101d58a3076e6286dede864875d9b1cffea5005a
James Skimming <gitcommits@skimming.net>
2013-04-16 11:13:53 +0100

    Completed the IsClassOrStructShould tests

--- src/Tests/AutoTest.ArgumentNullException.Tests/TypeFilter/IsClassOrStructShould.cs
+++ src/Tests/AutoTest.ArgumentNullException.Tests/TypeFilter/IsClassOrStructShould.cs
@@ -1,5 +1,6 @@
 ﻿namespace AutoTest.ArgNullEx.TypeFilter
 {
+    using System;
     using Xunit;
     using Xunit.Extensions;
 
@@ -9,6 +10,26 @@
         public void ReturnName(IsClassOrStruct sut)
         {
             Assert.Equal("IsClassOrStruct", sut.Name);
+        }
+
+        [Theory]
+        [InlineData(typeof(int), true)]
+        [InlineData(typeof(DateTime), true)]
+        [InlineData(typeof(int?), true)]
+        [InlineData(typeof(DateTime?), true)]
+        [InlineData(typeof(string), true)]
+        [InlineData(typeof(object), true)]
+        [InlineData(typeof(UriFormat), false)]
+        [InlineData(typeof(UriComponents), false)]
+        public void IncludeOnlyClassesAndStructs(Type type, bool expected)
+        {
+            ITypeFilter sut = new IsClassOrStruct();
+
+            // Act
+            bool actual = sut.IncludeType(type);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
