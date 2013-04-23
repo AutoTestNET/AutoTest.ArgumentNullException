2d341f9ad1668581fa2dda7b2accbeda8d9a458b
James Skimming <gitcommits@skimming.net>
2013-04-23 08:31:05 +0100

    Added the regex rule etensions

---
+++ src/AutoTest.ArgumentNullException/Filter/RegexRuleExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace AutoTest.ArgNullEx.Filter
+{
+    using System;
+
+    /// <summary>
+    /// Extension methods on <see cref="RegexRule"/>.
+    /// </summary>
+    public static class RegexRuleExtensions
+    {
+        /// <summary>
+        /// Checks whether the supplied <paramref name="type"/> matches the <paramref name="rule"/>.
+        /// </summary>
+        /// <param name="rule">The rule to match against the <paramref name="type"/>.</param>
+        /// <param name="type">The type to check if it matches the <paramref name="rule"/>.</param>
+        /// <returns><c>true</c> if the <paramref name="rule"/> matches against the <paramref name="type"/>; otherwise <c>false</c>.</returns>
+        public static bool MatchType(this RegexRule rule, Type type)
+        {
+            if (rule == null)
+                throw new ArgumentNullException("rule");
+            if (type == null)
+                throw new ArgumentNullException("type");
+            if (rule.Type == null)
+                throw new ArgumentException("The rule has a null Type regular expression.", "rule");
+
+            return rule.Type.IsMatch(type.FullName);
+        }
+    }
+}
---
+++ src/Tests/AutoTest.ArgumentNullException.Tests/Filter/RegexRuleExtensionsShould.cs
@@ -0,0 +1,49 @@
+﻿namespace AutoTest.ArgNullEx.Filter
+{
+    using System;
+    using System.Text.RegularExpressions;
+    using global::Xunit;
+
+    public class RegexRuleExtensionsShould
+    {
+        [Fact]
+        public void MatchAType()
+        {
+            // Arrange
+            var rule = new RegexRule(
+                typeof(RegexRuleExtensionsShould).Name + " match rule",
+                match: false,
+                type: new Regex(@".+\." + typeof(RegexRuleExtensionsShould).Name));
+
+            // Act
+            bool actual = rule.MatchType(typeof (RegexRuleExtensionsShould));
+
+            // Assert
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void NotMatchAType()
+        {
+            // Arrange
+            var rule = new RegexRule("Miss rule", match: false, type: new Regex("Miss"));
+
+            // Act
+            bool actual = rule.MatchType(typeof(RegexRuleExtensionsShould));
+
+            // Assert
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void ThrowIfNoTypeRuleForTypeMatch()
+        {
+            // Arrange
+            var rule = new RegexRule("Throw rule", match: false);
+
+            // Act/Assert
+            string paramName = Assert.Throws<ArgumentException>(() => rule.MatchType(typeof (RegexRuleExtensionsShould))).ParamName;
+            Assert.Equal("rule", paramName);
+        }
+    }
+}
