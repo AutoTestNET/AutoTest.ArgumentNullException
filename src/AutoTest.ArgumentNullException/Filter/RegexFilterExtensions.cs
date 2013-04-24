44d265492dac47f11c18a15454b3da960222493e
James Skimming <gitcommits@skimming.net>
2013-04-24 08:10:08 +0100

    Added Include/ExcludeMethod

--- src/AutoTest.ArgumentNullException/Filter/RegexFilterExtensions.cs
+++ src/AutoTest.ArgumentNullException/Filter/RegexFilterExtensions.cs
@@ -12,7 +12,7 @@
         /// Excludes the <paramref name="type"/> from checks for <see cref="ArgumentNullException"/>.
         /// </summary>
         /// <param name="filter">The <see cref="Regex"/> filter.</param>
-        /// <param name="type">The type to exclude.</param>
+        /// <param name="type">The type.</param>
         /// <returns>The <paramref name="filter"/>.</returns>
         public static IRegexFilter ExcludeType(this IRegexFilter filter, Type type)
         {
@@ -23,7 +23,7 @@
         /// Includes the <paramref name="type"/> for checks for <see cref="ArgumentNullException"/>. Overrides any type rules that may exclude the <paramref name="type"/>.
         /// </summary>
         /// <param name="filter">The <see cref="Regex"/> filter.</param>
-        /// <param name="type">The type to include.</param>
+        /// <param name="type">The type.</param>
         /// <returns>The <paramref name="filter"/>.</returns>
         public static IRegexFilter IncludeType(this IRegexFilter filter, Type type)
         {
@@ -31,10 +31,54 @@
         }
 
         /// <summary>
+        /// Excludes the <paramref name="methodName"/> from checks for <see cref="ArgumentNullException"/>.
+        /// </summary>
+        /// <param name="filter">The <see cref="Regex"/> filter.</param>
+        /// <param name="methodName">The method name.</param>
+        /// <param name="type">The type.</param>
+        /// <returns>The <paramref name="filter"/>.</returns>
+        public static IRegexFilter ExcludeMethod(this IRegexFilter filter, string methodName, Type type = null)
+        {
+            return filter.AddMethodRule(methodName, include: false, type: type);
+        }
+
+        /// <summary>
+        /// Includes the <paramref name="methodName"/> for checks for <see cref="ArgumentNullException"/>. Overrides any method rules that may exclude the <paramref name="methodName"/>.
+        /// </summary>
+        /// <param name="filter">The <see cref="Regex"/> filter.</param>
+        /// <param name="methodName">The method name.</param>
+        /// <param name="type">The type.</param>
+        /// <returns>The <paramref name="filter"/>.</returns>
+        public static IRegexFilter IncludeMethod(this IRegexFilter filter, string methodName, Type type = null)
+        {
+            return filter.AddMethodRule(methodName, include: false, type: type);
+        }
+
+        /// <summary>
+        /// Returns the <see cref="Regex"/> that matches on the <paramref name="type"/> if <paramref name="type"/> is not <c>null</c>; otherwise <c>null</c>.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns>The <see cref="Regex"/> that matches on the <paramref name="type"/> if <paramref name="type"/> is not <c>null</c>; otherwise <c>null</c>.</returns>
+        private static Regex GetTypeRegex(Type type = null)
+        {
+            return type == null ? null : new Regex(@"\A" + type.FullName + @"\z");
+        }
+
+        /// <summary>
+        /// Returns the <see cref="Regex"/> that matches on the <paramref name="methodName"/> if <paramref name="methodName"/> is not <c>null</c>; otherwise <c>null</c>.
+        /// </summary>
+        /// <param name="methodName">The method name.</param>
+        /// <returns>The <see cref="Regex"/> that matches on the <paramref name="methodName"/> if <paramref name="methodName"/> is not <c>null</c>; otherwise <c>null</c>.</returns>
+        private static Regex GetMethodRegex(string methodName = null)
+        {
+            return methodName == null ? null : new Regex(@"\A" + methodName + @"\z");
+        }
+
+        /// <summary>
         /// Adds the rule to include or exclude the <paramref name="type"/>.
         /// </summary>
         /// <param name="filter">The <see cref="Regex"/> filter.</param>
-        /// <param name="type">The type to include.</param>
+        /// <param name="type">The type.</param>
         /// <param name="include">A value indicating whether this is a include or exclude rule.</param>
         /// <returns>The <paramref name="filter"/>.</returns>
         private static IRegexFilter AddTypeRule(this IRegexFilter filter, Type type, bool include)
@@ -46,7 +90,36 @@
 
             var name = string.Concat(include ? "Include " : "Exclude ", type.Name);
 
-            filter.Rules.Add(new RegexRule(name, include: include, type: new Regex(@"\A" + type.FullName + @"\z")));
+            filter.Rules.Add(new RegexRule(
+                                 name,
+                                 include: include,
+                                 type: GetTypeRegex(type)));
+
+            return filter;
+        }
+
+        /// <summary>
+        /// Adds the rule to include or exclude the <paramref name="methodName"/>.
+        /// </summary>
+        /// <param name="filter">The <see cref="Regex"/> filter.</param>
+        /// <param name="methodName">The method name.</param>
+        /// <param name="include">A value indicating whether this is a include or exclude rule.</param>
+        /// <param name="type">The type.</param>
+        /// <returns>The <paramref name="filter"/>.</returns>
+        private static IRegexFilter AddMethodRule(this IRegexFilter filter, string methodName, bool include, Type type = null)
+        {
+            if (filter == null)
+                throw new ArgumentNullException("filter");
+            if (string.IsNullOrWhiteSpace(methodName))
+                throw new ArgumentNullException("methodName");
+
+            var name = string.Concat(include ? "Include " : "Exclude ", methodName);
+
+            filter.Rules.Add(new RegexRule(
+                                 name,
+                                 include: include,
+                                 type: GetTypeRegex(type),
+                                 method: GetMethodRegex(methodName)));
 
             return filter;
         }
--- src/Tests/AutoTest.ArgumentNullException.Tests/Filter/RegexFilterExtensionsShould.cs
+++ src/Tests/AutoTest.ArgumentNullException.Tests/Filter/RegexFilterExtensionsShould.cs
@@ -2,10 +2,15 @@
 {
     using System;
     using System.Linq;
+    using System.Reflection;
+    using Moq;
     using global::Xunit;
+    using global::Xunit.Extensions;
 
     public class RegexFilterExtensionsShould
     {
+        #region Exclude/Include Type
+
 // ReSharper disable UnusedParameter.Local
         private void AssertTypeRule(Func<Type, IRegexFilter> addMethod, bool expectedInclude)
         {
@@ -35,5 +40,82 @@
             // AAA
             AssertTypeRule(new RegexFilter().IncludeType, expectedInclude: true);
         }
+
+        #endregion Exclude/Include Type
+
+        #region Exclude/Include Method
+
+// ReSharper disable UnusedParameter.Local
+        private void AssertMethodRule(Func<string, Type, IRegexFilter> addMethod, MethodBase method, Type type, bool expectedInclude)
+        {
+            IRegexFilter actualFilter = addMethod(method.Name, type);
+
+            Assert.Same(addMethod.Target, actualFilter);
+            Assert.Equal(1, actualFilter.Rules.Count);
+            RegexRule addedRule = actualFilter.Rules.Single();
+            Assert.Equal(expectedInclude, addedRule.Include);
+            Assert.NotNull(addedRule.Method);
+            Assert.True(addedRule.MatchMethod(type ?? GetType(), method));
+            Assert.Null(addedRule.Parameter);
+
+            if (type == null)
+            {
+                Assert.Null(addedRule.Type);
+            }
+            else
+            {
+                Assert.NotNull(addedRule.Type);
+                Assert.True(addedRule.MatchType(type));
+            }
+        }
+// ReSharper restore UnusedParameter.Local
+
+        [Theory, AutoMock]
+        public void AddExcludeMethodRuleWithType(
+            Type type,
+            Mock<MethodBase> methodMock)
+        {
+            // Arrange
+            methodMock.SetupGet(m => m.Name).Returns("name" + Guid.NewGuid());
+
+            // Act/Assert
+            AssertMethodRule(new RegexFilter().ExcludeMethod, methodMock.Object, type, expectedInclude: false);
+        }
+
+        [Theory, AutoMock]
+        public void AddExcludeMethodRuleWithoutType(
+            Mock<MethodBase> methodMock)
+        {
+            // Arrange
+            methodMock.SetupGet(m => m.Name).Returns("name" + Guid.NewGuid());
+
+            // Act/Assert
+            AssertMethodRule(new RegexFilter().ExcludeMethod, methodMock.Object, type: null, expectedInclude: false);
+        }
+
+        [Theory, AutoMock]
+        public void AddIncludeMethodRuleWithType(
+            Type type,
+            Mock<MethodBase> methodMock)
+        {
+            // Arrange
+            methodMock.SetupGet(m => m.Name).Returns("name" + Guid.NewGuid());
+
+            // Act/Assert
+            AssertMethodRule(new RegexFilter().IncludeMethod, methodMock.Object, type, expectedInclude: false);
+        }
+
+        [Theory, AutoMock]
+        public void AddIncludeMethodRuleWithoutType(
+            Mock<MethodBase> methodMock)
+        {
+            // Arrange
+            methodMock.SetupGet(m => m.Name).Returns("name" + Guid.NewGuid());
+
+            // Act/Assert
+            AssertMethodRule(new RegexFilter().IncludeMethod, methodMock.Object, type: null, expectedInclude: false);
+        }
+
+        #endregion Exclude/Include Method
     }
 }
