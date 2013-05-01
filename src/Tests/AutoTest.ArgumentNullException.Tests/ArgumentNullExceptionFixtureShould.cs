cfc5939591c53ca737b81141749020e0fbb8ee91
James Skimming <gitcommits@skimming.net>
2013-05-01 18:31:11 +0100

    Tests all constructors of ArgumentNullExceptionFixture

--- src/Tests/AutoTest.ArgumentNullException.Tests/ArgumentNullExceptionFixtureShould.cs
+++ src/Tests/AutoTest.ArgumentNullException.Tests/ArgumentNullExceptionFixtureShould.cs
@@ -1,6 +1,8 @@
 ﻿namespace AutoTest.ArgNullEx
 {
+    using System.Collections.Generic;
     using System.Reflection;
+    using AutoTest.ArgNullEx.Filter;
     using Ploeh.AutoFixture;
     using Ploeh.AutoFixture.Xunit;
     using global::Xunit;
@@ -10,15 +12,32 @@
     {
         [Theory, AutoMock]
         public void InitializeDefaults(
-            Assembly expectedAssembly)
+            [Frozen] Assembly expectedAssembly,
+            [Modest] ArgumentNullExceptionFixture sut)
         {
-            // Arrange/Act
-            var sut = new ArgumentNullExceptionFixture(expectedAssembly);
-
-            // Assert
+            // AAA
             Assert.NotNull(sut.Fixture);
             Assert.Same(expectedAssembly, sut.AssemblyUnderTest);
             Assert.Equal(ArgumentNullExceptionFixture.DefaultBindingFlags, sut.BindingFlags);
+            Assert.NotNull(sut.Filters);
+            Assert.NotNull(sut.TypeFilters);
+            Assert.NotNull(sut.MethodFilters);
+            Assert.NotNull(sut.ParameterFilters);
+        }
+
+        [Theory, AutoMock]
+        public void InitializeInjectedDefaultsManual(
+            [Frozen] Assembly expectedAssembly,
+            [Frozen] IFixture expectedFixture)
+        {
+            // Arrange/Act
+            var sut = new ArgumentNullExceptionFixture(expectedAssembly, expectedFixture);
+
+            // Assert
+            Assert.Same(expectedFixture, sut.Fixture);
+            Assert.Same(expectedAssembly, sut.AssemblyUnderTest);
+            Assert.Equal(ArgumentNullExceptionFixture.DefaultBindingFlags, sut.BindingFlags);
+            Assert.NotNull(sut.Filters);
             Assert.NotNull(sut.TypeFilters);
             Assert.NotNull(sut.MethodFilters);
             Assert.NotNull(sut.ParameterFilters);
@@ -26,13 +45,15 @@
 
         [Theory, AutoMock]
         public void InitializeInjectedDefaults(
-            [Frozen] IFixture expectedFixture,
             [Frozen] Assembly expectedAssembly,
+            [Frozen] IFixture expectedFixture,
+            [Frozen] List<IFilter> filters,
             [Greedy] ArgumentNullExceptionFixture sut)
         {
             // AAA
             Assert.Same(expectedFixture, sut.Fixture);
             Assert.Same(expectedAssembly, sut.AssemblyUnderTest);
+            Assert.Same(filters, sut.Filters);
             Assert.Equal(ArgumentNullExceptionFixture.DefaultBindingFlags, sut.BindingFlags);
             Assert.NotNull(sut.TypeFilters);
             Assert.NotNull(sut.MethodFilters);
