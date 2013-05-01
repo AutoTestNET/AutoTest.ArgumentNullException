215066f924005e352c83c4e70068e43e14e71ba6
James Skimming <gitcommits@skimming.net>
2013-05-01 22:40:32 +0100

    Added the TypeMethodFiltering tests

---
+++ src/Tests/AutoTest.ArgumentNullException.Tests/Filter/TypeMethodFilteringShould.cs
@@ -0,0 +1,75 @@
+﻿namespace AutoTest.ArgNullEx.Filter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using Moq;
+    using global::Xunit;
+    using global::Xunit.Extensions;
+
+    public class TypeMethodFilteringShould
+    {
+        [Theory, AutoMock]
+        public void ApplyFiltersToAllMethods(
+            Assembly assembly,
+            Mock<IMethodFilter> filterMock)
+        {
+            // Arrange
+            const BindingFlags bindingFlags = ArgumentNullExceptionFixture.DefaultBindingFlags;
+            List<MethodBase> expectedMethods = GetAllMethods(assembly, bindingFlags).ToList();
+
+            // Confirm there are some methods in the assembly.
+            Assert.NotEmpty(expectedMethods);
+            filterMock.Setup(f => f.ExcludeMethod(It.IsAny<Type>(), It.IsAny<MethodBase>())).Returns(false);
+
+            // Act
+            List<MethodBase> actualMethods =
+                (from type in assembly.GetTypes()
+                 from method in type.GetMethods(bindingFlags, new[] { filterMock.Object })
+                 select method).ToList();
+
+            // Assert
+            filterMock.Verify(f => f.ExcludeMethod(It.IsAny<Type>(), It.IsAny<MethodBase>()),
+                              Times.Exactly(expectedMethods.Count));
+            Assert.Equal(expectedMethods.Count, actualMethods.Count);
+            Assert.False(expectedMethods.Except(actualMethods).Any());
+        }
+
+        private static IEnumerable<MethodBase> GetAllMethods(Assembly assembly, BindingFlags bindingFlags)
+        {
+            return
+                from type in assembly.GetTypes()
+                from method in type.GetMethods(bindingFlags)
+                                   .Cast<MethodBase>()
+                                   .Concat(type.GetConstructors(bindingFlags))
+                select method;
+        }
+
+        [Theory, AutoMock]
+        public void ApplyFiltersCorrectly(
+            Assembly assembly,
+            NotCompilerGenerated filter)
+        {
+            // Arrange
+            const BindingFlags bindingFlags = ArgumentNullExceptionFixture.DefaultBindingFlags;
+            List<MethodBase> compilerGeneratedMethods =
+                GetAllMethods(assembly, bindingFlags)
+                    .Where(m => m.IsCompilerGenerated())
+                    .ToList();
+
+            // Confirm there are methods to exclude.
+            Assert.NotEmpty(compilerGeneratedMethods);
+
+            // Act
+            List<MethodBase> actualMethods =
+                (from type in assembly.GetTypes()
+                 from method in type.GetMethods(bindingFlags, new[] { filter })
+                 select method).ToList();
+
+            // Assert
+            Assert.NotEmpty(actualMethods);
+            Assert.Equal(actualMethods.Count, actualMethods.Except(compilerGeneratedMethods).Count());
+        }
+    }
+}
