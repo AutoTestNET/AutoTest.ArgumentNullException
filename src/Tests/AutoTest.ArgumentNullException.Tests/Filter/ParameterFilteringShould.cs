aeae513ad7109c086e34240757a862b22e6be50d
James Skimming <gitcommits@skimming.net>
2013-05-02 16:14:53 +0100

    Added ParameterFiltering tests

---
+++ src/Tests/AutoTest.ArgumentNullException.Tests/Filter/ParameterFilteringShould.cs
@@ -0,0 +1,66 @@
+﻿namespace AutoTest.ArgNullEx.Filter
+{
+    using System;
+    using System.Reflection;
+    using Moq;
+    using global::Xunit;
+    using global::Xunit.Extensions;
+
+    public class ParameterFilteringShould
+    {
+        [Theory, AutoMock]
+        public void ApplyExclusionFilter(
+            Mock<IParameterFilter> filterMock,
+            Type type,
+            Mock<MethodBase> methodMock,
+            Mock<ParameterInfo> parameterMock)
+        {
+            // Arrange
+            parameterMock.Setup(p => p.ToString()).Returns(typeof(ParameterInfo).ToString);
+
+            parameterMock.SetupGet(p => p.Name)
+                .Returns(Guid.NewGuid().ToString())
+                .Verifiable();
+            methodMock.SetupGet(m => m.Name)
+                .Returns(Guid.NewGuid().ToString())
+                .Verifiable();
+            filterMock.Setup(f => f.ExcludeParameter(type, methodMock.Object, parameterMock.Object))
+                .Returns(true)
+                .Verifiable();
+            filterMock.SetupGet(f => f.Name)
+                      .Returns("mocked filter")
+                      .Verifiable();
+
+            // Act
+            bool actual = filterMock.Object.ApplyFilter(type, methodMock.Object, parameterMock.Object);
+
+            // Assert
+            filterMock.Verify();
+            parameterMock.Verify();
+            methodMock.Verify();
+            Assert.True(actual);
+        }
+
+        [Theory, AutoMock]
+        public void ApplyIncludionFilter(
+            Mock<IParameterFilter> filterMock,
+            Type type,
+            MethodBase method,
+            Mock<ParameterInfo> parameterMock)
+        {
+            // Arrange
+            parameterMock.Setup(p => p.ToString()).Returns(typeof(ParameterInfo).ToString);
+            filterMock.Setup(f => f.ExcludeParameter(type, method, parameterMock.Object))
+                .Returns(false)
+                .Verifiable();
+
+            // Act
+            bool actual = filterMock.Object.ApplyFilter(type, method, parameterMock.Object);
+
+            // Assert
+
+            filterMock.Verify();
+            Assert.False(actual);
+        }
+    }
+}
