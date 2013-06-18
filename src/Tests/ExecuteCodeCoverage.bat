b1452f4b868701763a668c376de5b48d60ef7612
James Skimming <gitcommits@skimming.net>
2013-06-18 14:36:04 +0100

    Reverted back to the earliest 3 version of AutoFixture

--- src/Tests/ExecuteCodeCoverage.bat
+++ src/Tests/ExecuteCodeCoverage.bat
@@ -1,11 +1,11 @@
-::"%~dp0..\packages\OpenCover.4.5.1528\OpenCover.Console.exe"
+::"%~dp0..\packages\OpenCover.4.5.1604\OpenCover.Console.exe"
 ::-register:user
 ::"-target:C:\git\AutoTest.ArgumentNullException\src\packages\xunit.runners.1.9.1\tools\xunit.console.clr4.exe"
 ::"-targetargs:C:\git\AutoTest.ArgumentNullException\src\Tests\Tests.xunit"
 ::"-filter:+[AutoTest.ArgumentNullException]* +[AutoTest.ArgumentNullException.Xunit]* +[AutoTest.ExampleLibrary]* -[AutoTest.ArgumentNullException.Xunit]System.Threading.Tasks.* -[AutoTest.ArgumentNullException]*ReflectionDiscoverableCollection*"
 ::"-output:C:\git\AutoTest.ArgumentNullException\src\Tests\CoverageResults.xml"
 
-"%~dp0..\packages\OpenCover.4.5.1528\OpenCover.Console.exe" -register:user "-target:C:\git\AutoTest.ArgumentNullException\src\packages\xunit.runners.1.9.1\tools\xunit.console.clr4.exe" "-targetargs:C:\git\AutoTest.ArgumentNullException\src\Tests\Tests.xunit" "-filter:+[AutoTest.ArgumentNullException]* +[AutoTest.ArgumentNullException.Xunit]* +[AutoTest.ExampleLibrary]* -[AutoTest.ArgumentNullException.Xunit]System.Threading.Tasks.* -[AutoTest.ArgumentNullException]*ReflectionDiscoverableCollection*" "-output:C:\git\AutoTest.ArgumentNullException\src\Tests\CoverageResults.xml"
+"%~dp0..\packages\OpenCover.4.5.1604\OpenCover.Console.exe" -register:user "-target:C:\git\AutoTest.ArgumentNullException\src\packages\xunit.runners.1.9.1\tools\xunit.console.clr4.exe" "-targetargs:C:\git\AutoTest.ArgumentNullException\src\Tests\Tests.xunit" "-filter:+[AutoTest.ArgumentNullException]* +[AutoTest.ArgumentNullException.Xunit]* +[AutoTest.ExampleLibrary]* -[AutoTest.ArgumentNullException.Xunit]System.Threading.Tasks.* -[AutoTest.ArgumentNullException]*ReflectionDiscoverableCollection*" "-output:C:\git\AutoTest.ArgumentNullException\src\Tests\CoverageResults.xml"
 
 ::"%~dp0..\packages\ReportGenerator.1.8.1.0\ReportGenerator.exe"
 ::"C:\git\AutoTest.ArgumentNullException\src\Tests\CoverageResults.xml"
