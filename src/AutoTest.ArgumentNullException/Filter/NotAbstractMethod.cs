cc05800f7563626fbb130d15ff48a07a8f417dc3
James Skimming <gitcommits@skimming.net>
2013-06-22 14:48:30 +0100

    Exclude abstract methods

---
+++ src/AutoTest.ArgumentNullException/Filter/NotAbstractMethod.cs
@@ -0,0 +1,27 @@
+﻿namespace AutoTest.ArgNullEx.Filter
+{
+    using System;
+    using System.Reflection;
+
+    /// <summary>
+    /// Filters out abstract methods.
+    /// </summary>
+    public class NotAbstractMethod : FilterBase, IMethodFilter
+    {
+        /// <summary>
+        /// Filters out abstract methods.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <param name="method">The method.</param>
+        /// <returns><c>true</c> if the <paramref name="method"/> should be excluded, otherwise <c>false</c>.</returns>
+        bool IMethodFilter.ExcludeMethod(Type type, MethodBase method)
+        {
+            if (type == null)
+                throw new ArgumentNullException("type");
+            if (method == null)
+                throw new ArgumentNullException("method");
+
+            return method.IsAbstract;
+        }
+    }
+}
---
+++ src/Tests/AutoTest.ArgumentNullException.Tests/Filter/NotAbstractMethodShould.cs
@@ -0,0 +1,56 @@
+﻿namespace AutoTest.ArgNullEx.Filter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using global::Xunit;
+    using global::Xunit.Extensions;
+
+    public class NotAbstractMethodShould
+    {
+        private abstract class TestClass
+        {
+            protected abstract void AbstractMethod();
+            protected virtual void NonAbstractMethod()
+            {
+            }
+        }
+
+        public static IEnumerable<object[]> AbstractMethods
+        {
+            get { return GetAbstractMethods(); }
+        }
+
+        internal static IEnumerable<object[]> GetAbstractMethods()
+        {
+            Type type = typeof (TestClass);
+            MethodInfo abstractMethod = type.GetMethod("AbstractMethod", BindingFlags.NonPublic | BindingFlags.Instance);
+            MethodInfo nonAbstractMethod = type.GetMethod("NonAbstractMethod", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            return new[]
+                {
+                    new object[] {type, abstractMethod, true},
+                    new object[] {type, nonAbstractMethod, false},
+                };
+        }
+
+        [Theory, AutoMock]
+        public void ReturnName(NotAbstractMethod sut)
+        {
+            Assert.Equal("NotAbstractMethod", sut.Name);
+        }
+
+        [Theory, PropertyData("AbstractMethods")]
+        public void ExcludeAbstractMethods(Type type, MethodInfo method, bool expected)
+        {
+            // Arrange
+            IMethodFilter sut = new NotAbstractMethod();
+
+            // Act
+            bool actual = sut.ExcludeMethod(type, method);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
