7a8b71a628e5522c0d20543a597782315991fe36
James Skimming <gitcommits@skimming.net>
2013-06-25 17:06:24 +0100

    Added tests to exercise Issue #6

---
+++ src/Tests/AutoTest.ExampleLibrary.Tests/Issues/Issue006/RequiresArgNullEx.cs
@@ -0,0 +1,17 @@
+﻿namespace AutoTest.ExampleLibrary.Issues.Issue006
+{
+    using System.Threading.Tasks;
+    using AutoTest.ArgNullEx;
+    using AutoTest.ArgNullEx.Xunit;
+    using Xunit.Extensions;
+
+    public class RequiresArgNullEx
+    {
+        [Theory, RequiresArgNullExAutoMoq(typeof(SpecialCharacters))]
+        [Include(Type = typeof(SpecialCharacters.InnerClass))]
+        public Task TestAllNullArguments(MethodData method)
+        {
+            return method.Execute();
+        }
+    }
+}
---
+++ src/Tests/AutoTest.ExampleLibrary/Issues/Issue006/SpecialCharacters.cs
@@ -0,0 +1,20 @@
+﻿namespace AutoTest.ExampleLibrary.Issues.Issue006
+{
+    using System;
+
+    /// <summary>
+    /// Test class used to demonstrate issue 6
+    /// https://github.com/AutoTestNET/AutoTest.ArgumentNullException/issues/6
+    /// </summary>
+    public class SpecialCharacters
+    {
+        public class InnerClass
+        {
+            static void AMethod(object input)
+            {
+                if (input == null)
+                    throw new ArgumentNullException("input");
+            }
+        }
+    }
+}
