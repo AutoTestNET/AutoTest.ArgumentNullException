0e0e05307154ae2079813ad6599bc49498ce7f96
James Skimming <JSkimming@users.noreply.github.com>
2014-06-03 18:07:46 +0100

    Incremented the version number to 0.6.0

--- src/Common/AssemblyInfoCommon.cs
+++ src/Common/AssemblyInfoCommon.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.4.0")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
