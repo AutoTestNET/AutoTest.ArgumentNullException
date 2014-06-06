f60e6540bc1119224a8e6a92533fa31fbabd96e1
James Skimming <JSkimming@users.noreply.github.com>
2014-06-06 23:19:17 +0100

    Easily exclude private methods using ExcludePrivateAttribute
    Closes #20

---
+++ src/AutoTest.ArgumentNullException.Xunit/ExcludePrivateAttribute.cs
@@ -0,0 +1,44 @@
+﻿namespace AutoTest.ArgNullEx.Xunit
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    /// <summary>
+    /// An attribute that can be applied to methods in an <see cref="RequiresArgumentNullExceptionAttribute"/>-driven
+    /// Theory to exclude private members from the test.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class ExcludePrivateAttribute : CustomizeAttribute, IArgNullExCustomization
+    {
+        /// <summary>
+        /// Gets a customization for a test method.
+        /// </summary>
+        /// <param name="method">The method to be customized.</param>
+        /// <returns>A customization for a test method.</returns>
+        /// <exception cref="ArgumentNullException">The <paramref name="method"/> parameter is
+        /// <see langword="null"/>.</exception>
+        public override IArgNullExCustomization GetCustomization(MethodInfo method)
+        {
+            if (method == null)
+                throw new ArgumentNullException("method");
+
+            return this;
+        }
+
+        /// <summary>
+        /// Customizes the specified <paramref name="fixture"/> by excluding private members.
+        /// </summary>
+        /// <param name="fixture">The fixture to customize.</param>
+        /// <exception cref="ArgumentNullException">The <paramref name="fixture"/> parameter is
+        /// <see langword="null"/>.</exception>
+        public virtual void Customize(IArgumentNullExceptionFixture fixture)
+        {
+            if (fixture == null)
+                throw new ArgumentNullException("fixture");
+
+            fixture.ExcludePrivate();
+        }
+    }
+}
---
+++ src/Tests/AutoTest.ExampleLibrary/Issues/Issue020/Mixture.cs
@@ -0,0 +1,31 @@
+﻿namespace AutoTest.ExampleLibrary.Issues.Issue020
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class Mixture
+    {
+        /// <summary>
+        /// Gets a value indicating if the <see cref="Mixture"/> has been tested.
+        /// </summary>
+        public static bool Tested { get; private set; }
+
+        public static string Public(string stringValue)
+        {
+            Tested = false;
+            string returnVal = Private(stringValue);
+
+            if (stringValue != null)
+                return returnVal;
+
+            Tested = true;
+            throw new ArgumentNullException("stringValue");
+        }
+
+        private static string Private(string stringValue)
+        {
+            return stringValue ?? string.Empty;
+        }
+    }
+}
