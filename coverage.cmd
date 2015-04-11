566764117140d7dc4144a571522b537fa45ad217
James Skimming <JSkimming@users.noreply.github.com>
2015-04-11 23:52:36 +0100

    Execute code coverage as part of the CI build

--- coverage.cmd
+++ coverage.cmd
@@ -6,5 +6,5 @@ if "%1" == "" (
    call "%~dp0build.cmd" Debug
 )
 
-echo %msbuild% "%~dp0src\Build\ExecuteUnitTests.proj" /nologo /verbosity:m /t:Rebuild
-%msbuild% "%~dp0src\Build\ExecuteUnitTests.proj" /nologo /verbosity:m /t:Rebuild
+echo %msbuild% "%~dp0src\Build\ExecuteUnitTests.proj" /nologo /verbosity:m /t:Rebuild /p:ExcludeXUnitTests=True
+%msbuild% "%~dp0src\Build\ExecuteUnitTests.proj" /nologo /verbosity:m /t:Rebuild /p:ExcludeXUnitTests=True
